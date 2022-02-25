9f0dd4024f5ebd9761f54a0c0f6a274a024d5896
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-02-25 21:30:45 +0200

    tt

--- migrations/sqls/20220225192615-initialize-up.sql
+++ migrations/sqls/20220225192615-initialize-up.sql
@@ -1,11 +1,8 @@
 /* Replace with your SQL commands */
-
-CREATE TABLE public.plant
+CREATE TABLE plant
 (
     id serial NOT NULL,
     plant_name VARCHAR(100),
     PRIMARY KEY (id)
 );
 
-ALTER TABLE IF EXISTS public.plant
-    OWNER to postgres;
