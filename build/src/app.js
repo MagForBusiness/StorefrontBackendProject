8b2ab6089a9237e3d5f50003beb879cb6da6bad9
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-02-25 19:02:01 +0200

    99

--- build/src/app.js
+++ build/src/app.js
@@ -1,19 +1,15 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var database_1 = __importDefault(require("./database"));
-var plants_1 = require("../src/models/plants");
-// const client = new Client({
-//   host: "localhost",
-//   port : 5432,
-//   user: "postgres",
-//   password: "1234",
-//   database: "plants_dev"
-// })
-database_1.default.connect();
-// let query = `Select * from "plants"`;
-var plants = new plants_1.plantsList();
-var result = plants.index();
-console.log(result);
+// import client from "./database"
+// import {plants,plantsList} from "../src/models/plants"
+// // const client = new Client({
+// //   host: "localhost",
+// //   port : 5432,
+// //   user: "postgres",
+// //   password: "1234",
+// //   database: "plants_dev"
+// // })
+// client.connect();
+// // let query = `Select * from "plants"`;
+// const plants =new plantsList();
+// const result=  plants.index();
+// console.log(result);
--- build/src/database.js
+++ build/src/database.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var dotenv_1 = __importDefault(require("dotenv"));
 var pg_1 = require("pg");
 dotenv_1.default.config();
-var _a = process.env, POSTGRES_HOST = _a.POSTGRES_HOST, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD;
+var _a = process.env, POSTGRES_HOST = _a.POSTGRES_HOST, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD, ENV = _a.ENV, POSTGRES_TEST_DB = _a.POSTGRES_TEST_DB;
 var client;
 if (ENV === 'test') {
     client = new pg_1.Pool({
@@ -23,5 +23,5 @@ if (ENV === 'dev') {
         user: POSTGRES_USER,
         password: POSTGRES_PASSWORD
     });
-    export default client;
 }
+exports.default = client;
