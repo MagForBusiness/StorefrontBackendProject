da6e489cda01809ae67ef6282930417ca8372308
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-02-25 21:02:56 +0200

    55

--- src/models/tests/plantsSpec.ts
+++ src/models/tests/plantsSpec.ts
@@ -6,5 +6,8 @@ describe("plant Model", () => {
     it('should have an index method', () => {
       expect(plants.index).toBeDefined();
     });
-  
+    it('index method should be return alist of plants',async () => {
+      const result =await plants.index();
+      expect(plants.index).toEqual([]);
+    });
 })
