ff471313481ce13c41b68402a496482b8863ead2
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-02-26 14:53:40 +0200

    11

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,23 @@ import {plants,plantsList} from "../src/models/plants";
 import bodyParser from 'body-parser';
 import express  from 'express';
 import cors from 'cors';
+
 //Create  application object with express()
 
 const app = express();
 
 const address:string = "0.0.0.0:3000";
+const corsOptions ={
+    origin: 'http://some3ddomian.com',
+    optionsSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
+// app.get ('/test-cors' , cors(corsOptions) , (req: express.Request, res: express.Response,next) => {
+//      res.json ({msg:`cors with the middle ware`);
+//      next();
+// })
+// ;
 
 app.use(bodyParser.json());
 
