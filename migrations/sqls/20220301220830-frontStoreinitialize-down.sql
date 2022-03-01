94d5f32b0f87edfd9d6a29435ce1ed45df9ddcb8
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-03-02 00:27:39 +0200

    ww

--- migrations/sqls/20220301220830-frontStoreinitialize-down.sql
+++ migrations/sqls/20220301220830-frontStoreinitialize-down.sql
@@ -1,14 +1,2 @@
 /* Replace with your SQL commands */
-CREATE TABLE users
-(
-    user_id serial NOT NULL,
-    firstName VARCHAR(100),
-    lastName VARCHAR(100),
-    username VARCHAR(100),
-    email VARCHAR(150),
-    userpassword VARCHAR(150),
-    token VARCHAR(450),
-    PRIMARY KEY (user_id)
-);
-
-
+DROP TABLE users;
--- migrations/sqls/20220301220830-frontStoreinitialize-up.sql
+++ migrations/sqls/20220301220830-frontStoreinitialize-up.sql
@@ -1,2 +1,14 @@
 /* Replace with your SQL commands */
-DROP TABLE users;
+CREATE TABLE users
+(
+    user_id serial NOT NULL,
+    firstName VARCHAR(100),
+    lastName VARCHAR(100),
+    username VARCHAR(100),
+    email VARCHAR(150),
+    userpassword VARCHAR(150),
+    token VARCHAR(450),
+    PRIMARY KEY (user_id)
+);
+
+
