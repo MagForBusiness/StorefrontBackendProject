e14c0e8a9a1f4ae60b7daeebaa47b12c39d94a2f
mostafa ahmed ghareeb <50140596+MagForBusiness@users.noreply.github.com>
2022-03-01 14:52:28 +0200

    r

---
+++ src/api/userprofile.ts
@@ -0,0 +1,1 @@
+// holding a user info from pg 
---
+++ src/routes/handlers/signinRoute.ts
@@ -0,0 +1,18 @@
+//login user route
+
+import express from 'express'
+//import { plants, plantsList } from '../../models/plants'
+
+// set up route
+export const LoginUser = express.Router()
+LoginUser.get('/', async (req: express.Request, res: express.Response) => {
+  try {
+    // const plants = new plantsList()
+    // const result = await plants.index()
+    // console.log(result)
+    res.send("the singin Route Here!")
+  } catch (err) {
+    res.status(400)
+    res.json(err)
+  }
+})
