8bdf6c844e33e2cc4bc1da5e7d5512102aa5dd35
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-03-02 21:40:39 +0200

    rr

--- build/routes/index.js
+++ build/routes/index.js
@@ -5,10 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var plantsRout_1 = require("./handlers/plantsRout");
+var singupRout_1 = require("./handlers/singupRout");
+var signinRoute_1 = require("./handlers/signinRoute");
 var router = express_1.default.Router();
 /* GET 1- show  plant route. */
 router.use('/show-plants', plantsRout_1.showPlants);
 /* GET 2-delete plant route. */
+// get register (NewUser) route
+router.use('/signup', singupRout_1.NewUser);
+// get Login (NewUser) route
+router.use('/signin', signinRoute_1.LoginUser);
 //show msg Main router Connect
 router.get('/', function (req, res) {
     res.send('Main router Connect!');
