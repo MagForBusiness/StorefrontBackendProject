2dff127a0b64d36aba95f8d0792fc601d11f9a8c
MagForBusiness <50140596+MagForBusiness@users.noreply.github.com>
2022-03-08 00:24:43 +0200

    555

--- build/database.js
+++ build/database.js
@@ -6,15 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var dotenv_1 = __importDefault(require("dotenv"));
 var pg_1 = require("pg");
 dotenv_1.default.config();
-var _a = process.env, POSTGRES_HOST = _a.POSTGRES_HOST, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD, ENV = _a.ENV, POSTGRES_TEST_DB = _a.POSTGRES_TEST_DB;
+var _a = process.env, POSTGRES_HOST = _a.POSTGRES_HOST, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD, ENV = _a.ENV, POSTGRES_TEST_DB = _a.POSTGRES_TEST_DB, POSTGRES_TEST_USER = _a.POSTGRES_TEST_USER, POSTGRES_TEST_PASSWORD = _a.POSTGRES_TEST_PASSWORD;
 var client;
 console.log(ENV);
 if (ENV === 'test') {
     client = new pg_1.Pool({
         host: POSTGRES_HOST,
         database: POSTGRES_TEST_DB,
-        user: POSTGRES_USER,
-        password: POSTGRES_PASSWORD,
+        user: POSTGRES_TEST_USER,
+        password: POSTGRES_TEST_PASSWORD,
     });
 }
 if (ENV === 'dev') {
--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 import { Pool } from 'pg'
 
 dotenv.config()
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, ENV, POSTGRES_TEST_DB } = process.env
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, ENV, POSTGRES_TEST_DB,POSTGRES_TEST_USER,POSTGRES_TEST_PASSWORD } = process.env
 
 let client
 console.log(ENV)
@@ -10,8 +10,8 @@ if (ENV === 'test') {
   client = new Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_TEST_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
+    user: POSTGRES_TEST_USER,
+    password: POSTGRES_TEST_PASSWORD,
   })
 }
 if (ENV === 'dev') {
