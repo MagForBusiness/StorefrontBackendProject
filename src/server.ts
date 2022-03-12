32a062e98d491587590eb4ce1033c068307d90ed
mostafa ahmed ghareeb <50140596+MagForBusiness@users.noreply.github.com>
2022-03-12 14:06:34 +0200

    orders

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import cors from 'cors'
-import router from './routes/index'
+import router from './routes/Routindex'
 
 //Create  application object with express()
 
